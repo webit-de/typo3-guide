2c6410b94e4a13e46c96bad3e3b73353c4da3c53
Thomas Deuling <info@coding.ms>
2017-03-26 12:51:25 +0200

    [TASK] ext_tables.php refactoring

--- ext_tables.php
+++ ext_tables.php
@@ -1,60 +1,40 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-if (TYPO3_MODE === 'BE') {
-	/**
-	 * Registers a Backend Module
-	 */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'Tx.' . $_EXTKEY,
-		'help',		// Make module a submodule of 'help'
-		'guide',	// Submodule key
-		'',			// Position
-		array(
-			'Guide' => 'list,ajaxRequest',
-		),
-		array(
-			'access' => 'user,group',
-			'iconIdentifier' => 'module-guide',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_guide.xlf',
-		)
-	);
-}
-// Register module icon
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-	'module-guide',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	array(
-		'source' => 'EXT:guide/ext_icon.svg'
-	)
-);
-$iconRegistry->registerIcon(
-	'module-guide-tour-core',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	array(
-		'source' => 'EXT:guide/Resources/Public/Icons/core_tours.svg'
-	)
-);
-$iconRegistry->registerIcon(
-	'module-guide-tour-page-module',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	array(
-		'source' => 'EXT:backend/Resources/Public/Icons/module-page.svg'
-	)
-);
-$iconRegistry->registerIcon(
-	'module-guide-tour-view-module',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	array(
-		'source' => 'EXT:viewpage/Resources/Public/Icons/module-viewpage.svg'
-	)
-);
-$iconRegistry->registerIcon(
-	'module-guide-tour-function-module',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	array(
-		'source' => 'EXT:func/Resources/Public/Icons/module-func.svg'
-	)
+defined('TYPO3_MODE') || die('Access denied.');
+call_user_func(
+    function ($extKey) {
+        // Add/register icons
+        if (TYPO3_MODE === 'BE') {
+            // Registers a Backend Module
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'Tx.' . $extKey,
+                'help',		// Make module a submodule of 'help'
+                'guide',	// Submodule key
+                '',			// Position
+                array(
+                    'Guide' => 'list,ajaxRequest',
+                ),
+                array(
+                    'access' => 'user,group',
+                    'icon' => 'EXT:guide/ext_icon.svg',
+                    'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_guide.xlf',
+                )
+            );
+            // register svg icons: identifier and filename
+            $iconsSvg = [
+                'module-guide-tour-function-module' => 'EXT:func/Resources/Public/Icons/module-func.svg',
+                'module-guide-tour-view-module' => 'EXT:viewpage/Resources/Public/Icons/module-viewpage.svg',
+                'module-guide-tour-page-module' => 'EXT:backend/Resources/Public/Icons/module-page.svg',
+                'module-guide-tour-core' => 'EXT:guide/Resources/Public/Icons/core_tours.svg'
+            ];
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            foreach ($iconsSvg as $identifier => $path) {
+                $iconRegistry->registerIcon(
+                    $identifier,
+                    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                    ['source' => $path]
+                );
+            }
+        }
+    },
+    $_EXTKEY
 );
